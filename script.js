0d20cf57626d56c1b7c3dd5835f4e8aee09c51b4
LoiChouette2036 <tristanbellanger10@gmail.com>
2024-01-20 22:21:15 +0900

    add an eventlistener when the mouse passe hover a square and it changes color

--- script.js
+++ script.js
@@ -15,7 +15,20 @@ for ( let j = 0 ; j < numberOfSquares ; j++) {
         container.appendChild(square);
 
     }
+
     
-    
+}
+
+//selection square element 
+var squares = document.getElementsByClassName("square");
+
+//add an event where we hover a square (so need a boucle for each square)
+for ( let k = 0 ; k < squares.length ;  k++ ){
+    squares[k].addEventListener("mouseover", function () {
+
+        console.log("hovered square");
+
+        squares[k].setAttribute('style','background-color: #FF0000;');
+    })
 }
     
