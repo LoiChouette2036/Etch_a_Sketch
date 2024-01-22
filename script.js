58052319a26574f08548958116e4876ef5667cc8
LoiChouette2036 <tristanbellanger10@gmail.com>
2024-01-22 18:59:30 +0900

    In the addeventListener clickOnButton, I faced a problem. \n When I called function to create a new square, the previous one was still here. \ So i just add a code line who delete the previous square : container.innerHTML.

--- script.js
+++ script.js
@@ -6,34 +6,41 @@ var container = document.getElementById("container");
 
 let numberOfSquares = 16;
 
-for ( let j = 0 ; j < numberOfSquares ; j++) {
-    for(let i = 0 ; i < numberOfSquares ; i++){
-        // create a div element
-        let square = document.createElement("div");
-
-        // add a class to put CSS style
-        square.className="square";
-
-        // add the square into container
-        container.appendChild(square);
-
-    }
-
+function creationSquare (numberOfSquares){
+    for ( let j = 0 ; j < numberOfSquares ; j++) {
+        for(let i = 0 ; i < numberOfSquares ; i++){
+            // create a div element
+            let square = document.createElement("div");
     
+            // add a class to put CSS style
+            square.className="square";
+    
+            // add the square into container
+            container.appendChild(square);
+    
+        } 
+    }
 }
 
-//selection square element 
-var squares = document.getElementsByClassName("square");
+creationSquare(numberOfSquares);
 
-//add an event where we hover a square (so need a boucle for each square)
-for ( let k = 0 ; k < squares.length ;  k++ ){
-    squares[k].addEventListener("mouseover", function () {
 
-        console.log("hovered square");
+function game(){
+    //selection square element 
+    var squares = document.getElementsByClassName("square");
 
-        squares[k].setAttribute('style','background-color: #FF0000;');
-    })
+    //add an event where we hover a square (so need a boucle for each square)
+    for ( let k = 0 ; k < squares.length ;  k++ ){
+        squares[k].addEventListener("mouseover", function () {
+
+            console.log("hovered square");
+
+            squares[k].setAttribute('style','background-color: #FF0000;');
+        })
+    }
 }
+
+game();
     
 var clickOnButton = document.querySelector('.my_button')
 
@@ -46,5 +53,11 @@ clickOnButton.addEventListener('click', function (){
         return 0;
     } else {
         console.log("you choose number : " + userInput )
+
+        // erase the previous square.
+        container.innerHTML = ''; 
+        
+        creationSquare(userInput);
+        game();
     }
 } )
